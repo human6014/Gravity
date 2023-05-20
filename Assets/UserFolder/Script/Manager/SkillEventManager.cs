41cdccc4e38da6bbe1363df55f81490ab4dfd1b9
권승현 <84135346+human6014@users.noreply.github.com>
2023-05-21 03:59:49 +0900

    Feat : Lightmap, Skillevent
    라이트맵 Bake
    SkillEvent 구조 짜는중

--- Assets/UserFolder/Script/Manager/SkillEventManager.cs
+++ Assets/UserFolder/Script/Manager/SkillEventManager.cs
@@ -5,20 +5,64 @@ using UI.Event;
 
 namespace UI.Manager
 {
+    [System.Serializable]
+    public struct FixedNonSpecificEvent
+    {
+        public int m_EventCount;
+        public Scriptable.UI.EventType m_EventType;
+    }
+
+    [System.Serializable]
+    public struct FixedSpecificEvent
+    {
+        public int m_EventCount;
+        public Event.SkillEvent[] m_SkillEvent;
+    }
+
+    [System.Serializable]
+    public class SkillEvent
+    {
+        public Scriptable.UI.EventType m_EventType;
+        public Event.SkillEvent[] m_SkillEvent;
+    }
+
     public class SkillEventManager : MonoBehaviour
     {
         [SerializeField] private SettingUIManager m_SettingUIManager;
         [SerializeField] private PlayerData m_PlayerData;
         [SerializeField] private RectTransform m_SkillPos;
-        [SerializeField] private SkillEvent[] m_SkillEvents;
+
+
+        [SerializeField] private FixedNonSpecificEvent[] m_FixedNonSpecificEvents;
+        [SerializeField] private FixedSpecificEvent[] m_FixedSpecificEvents;
+        //위 둘 타이밍 안곂쳐야 함
+
+        [SerializeField] private SkillEvent[] m_SkillEvent;
+
+        private List<int> m_FixedEventTiming;
 
         private Animator m_Animator;
 
         private int m_EventCount = 0;
+        private int [] m_SkillLevel;
 
         private void Awake()
         {
             m_Animator = m_SkillPos.GetComponent<Animator>();
+            SetFixedEvent();
+        }
+
+        private void SetFixedEvent()
+        {
+            m_FixedEventTiming = new List<int>();
+
+            foreach (FixedNonSpecificEvent fse in m_FixedNonSpecificEvents)
+                m_FixedEventTiming.Add(fse.m_EventCount);
+
+            foreach (FixedSpecificEvent fse in m_FixedSpecificEvents)
+                m_FixedEventTiming.Add(fse.m_EventCount);
+
+            m_FixedEventTiming.Sort();
         }
 
         [ContextMenu("OccurSkillEvent")]
@@ -28,13 +72,23 @@ namespace UI.Manager
             m_Animator.SetTrigger("Show");
             m_SettingUIManager.PauseMode(true);
             m_EventCount++;
+
+            BatchSkillUI();
         }
+
+        private void BatchSkillUI()
+        {
+
+        }
+
         [ContextMenu("EndSkillEvent")]
         public void EndSkillEvent()
         {
             m_Animator.SetTrigger("Hide");
-            //m_SkillPos.gameObject.SetActive(false);
             m_SettingUIManager.PauseMode(false);
         }
+
+        public void EndHideAnimation() => m_SkillPos.gameObject.SetActive(false);
+        
     }
 }
--- Assets/UserFolder/Script/UI/Event/GetWeaponEvent.cs
+++ Assets/UserFolder/Script/UI/Event/GetWeaponEvent.cs
@@ -6,6 +6,24 @@ namespace UI.Event
 {
     public class GetWeaponEvent : SkillEvent
     {
+        [SerializeField] private PlayerData m_PlayerData;
 
+        [SerializeField] private int m_SlotNumber;
+        [SerializeField] private int m_WeaponIndex;
+
+
+        public override void Init()
+        {
+            base.Init();
+
+
+        }
+
+        public override void DoSkill()
+        {
+            base.DoSkill();
+
+
+        }
     }
 }
