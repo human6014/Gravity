721f50ae62499118a403d3574953d675ee810ff5
권승현 <84135346+human6014@users.noreply.github.com>
2023-08-12 02:47:03 +0900

    Feat : SP2 idle, attack dir, animation
    SpecialMonster2 목표와 근접 시 정지 상태 애니메이션, 회전 등 적용
    일반 공격 정면 각도 내에서만 가능하도록 변경
    일반 공격 애니메이션 변경

---
+++ Assets/UserFolder/Script/Entity/Unit/SpecialMonster/SpecialMonster2/GrabController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController : MonoBehaviour
+{
+    [SerializeField] private Transform[] m_GrabTentacles;
+}
